7c8e9c3b37ba4ac54c7b43294bb8237ea420a7c0
Harshiiitha <harshithareddy829@gmail.com>
2019-06-11 16:50:00 +0530

    added dockercompose and parent pom

--- muzix-service/src/main/java/com/stackroute/muzixservice/prefill/CommandLineRunnerDemo.java
+++ muzix-service/src/main/java/com/stackroute/muzixservice/prefill/CommandLineRunnerDemo.java
@@ -6,19 +6,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CommandLineRunnerDemo implements CommandLineRunner {
 
+    @Autowired
+    private Environment environment;
 
-    @Value("${track2.id}")
+
+   /* @Value("${track2.id}")
     private String id;
     @Value("${track2.name}")
     private String name;
     @Value("${track2.comment}")
     private String comment;
-
+*/
     @Autowired
     MuzixRepository muzixRepository;
     Track track=new Track();
@@ -26,9 +30,9 @@ public class CommandLineRunnerDemo implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        track.setTrackId(id);
-        track.setTrackName(name);
-        track.setTrackComments(comment);
+        track.setTrackId(environment.getProperty("track2.id"));
+        track.setTrackName(environment.getProperty("track2.name"));
+        track.setTrackComments(environment.getProperty("track2.comment"));
         muzixRepository.save(track);
 
     }
